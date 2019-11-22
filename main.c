b766b846db3dcec18e4990f58184e82641f080d4
manik922001 <57584417+manik922001@users.noreply.github.com>
2019-11-23 00:48:14 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("enter value");
+    scanf("%d\n%d",&a,&b);
+    (a<b)?printf("b is greater"):printf("a is greater");
+
+    return 0;
+}
